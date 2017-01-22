52b5e23083a48dfbfdf90d63c7822bf77842b2b7
shawniverson <shawniverson@gmail.com>
2017-01-22 14:00:32 -0500

    service configuration

---
+++ build/eFa/service-config-4.0.0.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+#-----------------------------------------------------------------------------#
+# eFa 4.0.0 initial service-configuration script
+#-----------------------------------------------------------------------------#
+# Copyright (C) 2013~2017 https://efa-project.org
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#-----------------------------------------------------------------------------#
+
+#-----------------------------------------------------------------------------#
+# Source the settings file
+#-----------------------------------------------------------------------------#
+source /usr/src/eFa/eFa-settings.inc
+#-----------------------------------------------------------------------------#
+
+#-----------------------------------------------------------------------------#
+# Start configuration of services
+#-----------------------------------------------------------------------------#
+
+# These services we really don't need.
+systemctl disable lvm2-monitor
+systemctl disable mdmonitor
+systemctl disable smartd
+systemctl disable abrtd
+
+# These services we disable for now and enable them after EFA-Init.
+# Most of these are not enabled by default but add them here just to
+# make sure we don't forget them at EFA-Init.
+chkconfig mailscanner off
+systemctl disable postfix
+systemctl disable httpd
+systemctl disable mariadb
+#chkconfig saslauthd off
+systemctl disable crond
+systemctl disable clamd
+chkconfig sqlgrey off
+#chkconfig mailgraph-init off
+chkconfig adcc off
+#chkconfig webmin off
+systemctl disable unbound
+#chkconfig munin-node off
+systemctl disable chronyd
--- rpm/specs/eFa/eFa4.spec
+++ rpm/specs/eFa/eFa4.spec
@@ -362,6 +362,7 @@ if [ "$1" = "1" ]; then
     /bin/sh %{_usrsrc}/eFa/pyzor-config-4.0.0.sh | tee -a $logfile
     /bin/sh %{_usrsrc}/eFa/razor-config-4.0.0.sh | tee -a $logfile
     /bin/sh %{_usrsrc}/eFa/unbound-config-4.0.0.sh | tee -a $logfile
+    /bin/sh %{_usrsrc}/eFa/service-config-4.0.0.sh | tee -a $logfile
     
     echo "eFa-%{version}" > %{_sysconfdir}/eFa-Version
     echo "Build completed!"
