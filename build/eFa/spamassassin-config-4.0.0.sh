3a5abfa3b3522038760843b95fe91dc8dc3c80c2
shawniverson <shawniverson@gmail.com>
2017-02-11 13:27:00 -0500

    Fix typo in spamassassin config

--- build/eFa/spamassassin-config-4.0.0.sh
+++ build/eFa/spamassassin-config-4.0.0.sh
@@ -105,7 +105,7 @@ chmod +x trim-sql-awl-weekly
 # Create .spamassassin directory (error reported in lint test)
 mkdir -p /var/www/.spamassassin
 chown postfix:mtagroup /var/www/.spamassassin
-mkdir -p /user/share/httpd/.spamassassin
+mkdir -p /usr/share/httpd/.spamassassin
 chown postfix:mtagroup /usr/share/httpd/.spamassassin
 
 # Issue #82 re2c spamassassin rule complilation
