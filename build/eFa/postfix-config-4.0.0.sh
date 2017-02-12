4d1bf80c43b9da58286b789addfd0ec703e66637
shawniverson <shawniverson@gmail.com>
2017-02-12 14:41:49 -0500

    Enable ipv6 in postfix

--- build/eFa/postfix-config-4.0.0.sh
+++ build/eFa/postfix-config-4.0.0.sh
@@ -31,7 +31,7 @@ echo "Configuring postfix..."
 
 mkdir /etc/postfix/ssl
 echo /^Received:/ HOLD>>/etc/postfix/header_checks
-postconf -e "inet_protocols = ipv4"
+postconf -e "inet_protocols = ipv4, ipv6"
 postconf -e "inet_interfaces = all"
 postconf -e "mynetworks = 127.0.0.0/8"
 postconf -e "header_checks = regexp:/etc/postfix/header_checks"
