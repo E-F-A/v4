bf8241ebdc903d983457f9aaa840cc1f9f2179cd
Shawn Iverson <shawniverson@efa-project.org>
2018-01-14 08:50:23 -0500

    Update razor-config

--- build/eFa/razor-config-4.0.0.sh
+++ build/eFa/razor-config-4.0.0.sh
@@ -2,7 +2,7 @@
 #-----------------------------------------------------------------------------#
 # eFa 4.0.0 initial razor-configuration script
 #-----------------------------------------------------------------------------#
-# Copyright (C) 2013~2017 https://efa-project.org
+# Copyright (C) 2013~2018 https://efa-project.org
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
