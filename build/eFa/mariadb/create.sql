d220896b33918e1d19a6c5f2dcaed74ade22e729
Shawn Iverson <shawniverson@efa-project.org>
2018-01-14 08:43:08 -0500

    Update create.sql

--- build/eFa/mariadb/create.sql
+++ build/eFa/mariadb/create.sql
@@ -20,10 +20,12 @@ USE `mailscanner`;
 --
 
 CREATE TABLE IF NOT EXISTS `audit_log` (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `user` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `user` varchar(191) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `ip_address` varchar(15) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `action` text COLLATE utf8_unicode_ci NOT NULL
+  `action` text COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -37,7 +39,7 @@ CREATE TABLE IF NOT EXISTS `autorelease` (
   `msg_id` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `uid` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -46,13 +48,13 @@ CREATE TABLE IF NOT EXISTS `autorelease` (
 --
 
 CREATE TABLE IF NOT EXISTS `blacklist` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `to_address` text COLLATE utf8_unicode_ci,
   `to_domain` text COLLATE utf8_unicode_ci,
   `from_address` text COLLATE utf8_unicode_ci,
   PRIMARY KEY (`id`),
   UNIQUE KEY `blacklist_uniq` (`to_address`(100),`from_address`(100))
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -61,6 +63,7 @@ CREATE TABLE IF NOT EXISTS `blacklist` (
 --
 
 CREATE TABLE IF NOT EXISTS `inq` (
+  `inq_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `id` mediumtext COLLATE utf8_unicode_ci,
   `cdate` date DEFAULT NULL,
   `ctime` time DEFAULT NULL,
@@ -73,6 +76,7 @@ CREATE TABLE IF NOT EXISTS `inq` (
   `attempts` mediumtext COLLATE utf8_unicode_ci,
   `lastattempt` mediumtext COLLATE utf8_unicode_ci,
   `hostname` mediumtext COLLATE utf8_unicode_ci,
+  PRIMARY KEY (`inq_id`),
   KEY `inq_hostname` (`hostname`(50))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -83,7 +87,8 @@ CREATE TABLE IF NOT EXISTS `inq` (
 --
 
 CREATE TABLE IF NOT EXISTS `maillog` (
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `maillog_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NULL,
   `id` mediumtext COLLATE utf8_unicode_ci,
   `size` bigint(20) DEFAULT '0',
   `from_address` mediumtext COLLATE utf8_unicode_ci,
@@ -104,7 +109,7 @@ CREATE TABLE IF NOT EXISTS `maillog` (
   `sascore` decimal(7,2) DEFAULT '0.00',
   `spamreport` mediumtext COLLATE utf8_unicode_ci,
   `virusinfected` tinyint(1) DEFAULT '0',
-  `nameinfected` tinyint(1) DEFAULT '0',
+  `nameinfected` tinyint(2) DEFAULT '0',
   `otherinfected` tinyint(1) DEFAULT '0',
   `report` mediumtext COLLATE utf8_unicode_ci,
   `ismcp` tinyint(1) DEFAULT '0',
@@ -119,22 +124,24 @@ CREATE TABLE IF NOT EXISTS `maillog` (
   `time` time DEFAULT NULL,
   `headers` mediumtext COLLATE utf8_unicode_ci,
   `quarantined` tinyint(1) DEFAULT '0',
+  `rblspamreport` mediumtext COLLATE utf8_unicode_ci DEFAULT NULL,
+  `token` CHAR(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `released` tinyint(1) DEFAULT '0',
+  `salearn` tinyint(1) DEFAULT '0',
+  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`maillog_id`),
   KEY `maillog_datetime_idx` (`date`,`time`),
   KEY `maillog_id_idx` (`id`(20)),
   KEY `maillog_clientip_idx` (`clientip`(20)),
-  KEY `maillog_from_idx` (`from_address`(200)),
-  KEY `maillog_to_idx` (`to_address`(200)),
+  KEY `maillog_from_idx` (`from_address`(191)),
+  KEY `maillog_to_idx` (`to_address`(191)),
   KEY `maillog_host` (`hostname`(30)),
   KEY `from_domain_idx` (`from_domain`(50)),
   KEY `to_domain_idx` (`to_domain`(50)),
   KEY `maillog_quarantined` (`quarantined`),
   KEY `timestamp_idx` (`timestamp`)
+  /*!50604 , FULLTEXT KEY `subject_idx` (`subject`) */
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Indexes for table `maillog`
---
--- ALTER TABLE `maillog` ADD FULLTEXT KEY `subject_idx` (`subject`);
 
 -- --------------------------------------------------------
 
@@ -155,6 +162,7 @@ CREATE TABLE IF NOT EXISTS `mcp_rules` (
 --
 
 CREATE TABLE IF NOT EXISTS `mtalog` (
+  `mtalog_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `timestamp` datetime DEFAULT NULL,
   `host` mediumtext COLLATE utf8_unicode_ci,
   `type` mediumtext COLLATE utf8_unicode_ci,
@@ -163,6 +171,7 @@ CREATE TABLE IF NOT EXISTS `mtalog` (
   `dsn` mediumtext COLLATE utf8_unicode_ci,
   `status` mediumtext COLLATE utf8_unicode_ci,
   `delay` time DEFAULT NULL,
+  PRIMARY KEY (`mtalog_id`),
   UNIQUE KEY `mtalog_uniq` (`timestamp`,`host`(10),`type`(10),`msg_id`,`relay`(20)),
   KEY `mtalog_timestamp` (`timestamp`),
   KEY `mtalog_type` (`type`(10)),
@@ -189,6 +198,7 @@ CREATE TABLE IF NOT EXISTS `mtalog_ids` (
 --
 
 CREATE TABLE IF NOT EXISTS `outq` (
+  `outq_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `id` mediumtext COLLATE utf8_unicode_ci,
   `cdate` date DEFAULT NULL,
   `ctime` time DEFAULT NULL,
@@ -201,6 +211,7 @@ CREATE TABLE IF NOT EXISTS `outq` (
   `attempts` mediumtext COLLATE utf8_unicode_ci,
   `lastattempt` mediumtext COLLATE utf8_unicode_ci,
   `hostname` mediumtext COLLATE utf8_unicode_ci,
+  PRIMARY KEY (`outq_id`),
   KEY `outq_hostname` (`hostname`(50))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -211,11 +222,13 @@ CREATE TABLE IF NOT EXISTS `outq` (
 --
 
 CREATE TABLE IF NOT EXISTS `saved_filters` (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` mediumtext COLLATE utf8_unicode_ci NOT NULL,
   `col` mediumtext COLLATE utf8_unicode_ci NOT NULL,
   `operator` mediumtext COLLATE utf8_unicode_ci NOT NULL,
   `value` mediumtext COLLATE utf8_unicode_ci NOT NULL,
   `username` mediumtext COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
   UNIQUE KEY `unique_filters` (`name`(20),`col`(20),`operator`(20),`value`(20),`username`(20))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -234,32 +247,26 @@ CREATE TABLE IF NOT EXISTS `sa_rules` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `spamscores`
---
-
-CREATE TABLE IF NOT EXISTS `spamscores` (
-  `user` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `lowspamscore` decimal(10,0) NOT NULL DEFAULT '0',
-  `highspamscore` decimal(10,0) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`user`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `users`
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `id` BIGINT NOT NULL AUTO_INCREMENT UNIQUE KEY,
+  `username` varchar(191) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `fullname` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `type` enum('A','D','U','R','H') COLLATE utf8_unicode_ci DEFAULT NULL,
   `quarantine_report` tinyint(1) DEFAULT '0',
-  `spamscore` tinyint(4) DEFAULT '0',
-  `highspamscore` tinyint(4) DEFAULT '0',
+  `spamscore` float DEFAULT '0',
+  `highspamscore` float DEFAULT '0',
   `noscan` tinyint(1) DEFAULT '0',
   `quarantine_rcpt` varchar(60) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `resetid` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `resetexpire` bigint(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `lastreset` bigint(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `login_expiry` bigint(20) COLLATE utf8_unicode_ci DEFAULT '-1',
+  `last_login` bigint(20) COLLATE utf8_unicode_ci DEFAULT '-1',
+  `login_timeout` smallint(5) COLLATE utf8_unicode_ci DEFAULT '-1',
   PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -270,10 +277,12 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 CREATE TABLE IF NOT EXISTS `user_filters` (
-  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username` varchar(191) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `filter` mediumtext COLLATE utf8_unicode_ci,
   `verify_key` varchar(32) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `active` enum('N','Y') COLLATE utf8_unicode_ci DEFAULT 'N',
+  PRIMARY KEY (`id`),
   KEY `user_filters_username_idx` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -284,13 +293,15 @@ CREATE TABLE IF NOT EXISTS `user_filters` (
 --
 
 CREATE TABLE IF NOT EXISTS `whitelist` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` bigint(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `to_address` mediumtext COLLATE utf8_unicode_ci,
   `to_domain` mediumtext COLLATE utf8_unicode_ci,
   `from_address` mediumtext COLLATE utf8_unicode_ci,
   PRIMARY KEY (`id`),
   UNIQUE KEY `whitelist_uniq` (`to_address`(100),`from_address`(100))
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
