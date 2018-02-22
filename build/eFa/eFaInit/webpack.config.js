b4ae3f6a722b1358d523bb83160c44f9a3f74742
Shawn Iverson <shawniverson@gmail.com>
2018-02-22 05:53:03 -0500

    Add webpack configuration

---
+++ build/eFa/eFaInit/webpack.config.js
@@ -0,0 +1,33 @@
+// webpack.config.js
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+    // the project directory where all compiled assets will be stored
+    .setOutputPath('web/build/')
+
+    // the public path used by the web server to access the previous directory
+    .setPublicPath('/build')
+
+    // will create web/build/app.js and web/build/app.css
+    .addEntry('app', './assets/js/app.js')
+
+    // allow sass/scss files to be processed
+    .enableSassLoader()
+
+    // allow legacy applications to use $/jQuery as a global variable
+    .autoProvidejQuery()
+
+    .enableSourceMaps(!Encore.isProduction())
+
+    // empty the outputPath dir before each build
+    .cleanupOutputBeforeBuild()
+
+    // show OS notifications when builds finish/fail
+    .enableBuildNotifications()
+
+    // create hashed filenames (e.g. app.abc123.css)
+    // .enableVersioning()
+;
+
+// export the final configuration
+module.exports = Encore.getWebpackConfig();
