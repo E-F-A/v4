376b9655264663dc9829a7f565befc595e4e3d83
Shawn Iverson <shawniverson@gmail.com>
2018-03-04 15:35:57 -0500

    Language selection initial

--- build/eFa/eFaInit/src/AppBundle/Controller/DefaultController.php
+++ build/eFa/eFaInit/src/AppBundle/Controller/DefaultController.php
@@ -3,19 +3,19 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+class DefaultController extends AbstractController
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/{_locale}",
+     *     name="homepage",
+     *     defaults={"_locale": "en"}
+     * )
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('default/index.html.twig');
     }
 }
