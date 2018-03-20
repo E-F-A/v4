735f16e3b8ab8603e02d46528340dd23ea06c610
root <root@localhost.localdomain>
2018-03-20 23:34:19 +0000

    Reorganize repo for local building

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+######################################################################
+# eFa 4.0.0 Build script for local development builds
+######################################################################
+# Copyright (C) 2018  https://efa-project.org
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#######################################################################
+
+yum -y install rpm-build rpmdevtools
+yum -y install gcc-c++ gcc
+mkdir -p rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+echo '%_topdir $(pwd)/rpmbuild' > ~/.rpmmacros
