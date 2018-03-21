36d0c79a4641d48bd2e7974ec25ee9890b514fb8
root <shawniverson@efa-project.org>
2018-03-21 01:27:04 +0000

    Update sources and specs

--- build.sh
+++ build.sh
@@ -18,7 +18,24 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #######################################################################
 
-yum -y install rpm-build rpmdevtools
-yum -y install gcc-c++ gcc
+yum -y install epel-release
+yum update
+yum -y install rpm-build rpmdevtools gcc-c++ gcc perl-Net-DNS perl-NetAddr-IP openssl-devel perl-Test-Pod perl-HTML-Parser perl-Archive-Tar perl-devel perl-libwww-perl perl-DB_File perl-Mail-SPF perl-Encode-Detect perl-IO-Socket-INET6 perl-Mail-DKIM perl-Net-DNS-Resolver-Programmable perl-Parse-RecDescent perl-Inline perl-Test-Manifest perl-YAML perl-ExtUtils-CBuilder perl-Module-Build perl-IO-String perl-Geo-IP perl-Net-CIDR-Lite perl-Sys-Hostname-Long perl-Net-IP perl-Net-Patricia perl-Data-Dump perl-generators libicu-devel openldap-devel mysql-devel postgresql-devel  sqlite-devel tinycdb-devel 
 mkdir -p rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
 echo '%_topdir $(pwd)/rpmbuild' > ~/.rpmmacros
+cd rpmbuild/SPECS
+rpmbuild -ba clamav-unofficial-sigs.spec
+rpmbuild -ba perl-IP-Country.spec
+echo "n" | rpmbuild -ba perl-libnet.spec
+rpm -ivh ../RPMS/x86_64/perl-libnet-3.11-1.eFa.el7.centos.x86_64.rpm
+rpmbuild -ba perl-Geography-Countries.spec
+rpm -ivh ../RPMS/x86_64/perl-Geography-Countries-2009041301-1.eFa.el7.centos.x86_64.rpm
+rpm -ivh ../RPMS/x86_64/perl-IP-Country-2.28-1.eFa.el7.centos.x86_64.rpm
+rpmbuild -ba perl-Text-Balanced.spec
+rpm -ivh ../RPMS/x86_64/perl-Text-Balanced-2.03-1.eFa.el7.centos.x86_64.rpm
+rpmbuild -ba Spamassassin.spec
+rpmbuild -ba dcc.spec
+rpmbuild -ba perl-Mail-SPF-Query.spec
+rpmbuild -ba mailwatch-1.2.7.spec
+rpmbuild -ba postfix-3.3.0.spec
+rpmbuild -ba unrar-5.6.1.spec
