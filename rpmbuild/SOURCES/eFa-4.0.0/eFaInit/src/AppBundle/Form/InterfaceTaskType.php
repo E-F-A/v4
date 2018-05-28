2043a8ad0016ebbb892eb503190d66c8a43b10a3
Shawn Iverson <shawniverson@efa-project.org>
2018-05-28 17:55:48 -0400

    Interface selection logic
    Former-commit-id: ae054e037656ad22120bc431932b398433cf0c34

---
+++ rpmbuild/SOURCES/eFa-4.0.0/eFaInit/src/AppBundle/Form/InterfaceTaskType.php
@@ -0,0 +1,42 @@
+<?php
+// src/AppBundle/Form/InterfaceTaskType.php
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\eFaInitTask;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class InterfaceTaskType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('Interface', ChoiceType::class, array(
+                'choices'  => $options['varChoices']
+            ))
+            ->add('NextHidden', SubmitType::class, array(
+                'validation_groups' => array($options['varProperty']),
+            ))
+            ->add('Next', SubmitType::class, array(
+                'validation_groups' => array($options['varProperty']),
+            ))
+            ->add('Back', SubmitType::class, array(
+                'validation_groups' => false
+            ))
+        ;
+    }
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => eFaInitTask::class,
+            'csrf_token_id' => 'interface_task'
+        ));
+        $resolver->setRequired('varChoices');
+        $resolver->setRequired('varProperty');
+    }
+}
+?>
