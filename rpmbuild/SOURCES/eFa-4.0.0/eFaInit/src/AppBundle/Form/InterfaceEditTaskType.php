59c80d12b9d399583683a72578b9330d128a3493
Shawn Iverson <shawniverson@efa-project.org>
2018-05-28 17:55:48 -0400

    Interface selection logic
    Former-commit-id: 294fb9a19209626b9dcd53cb7f259cbf83cb637a [formerly ae054e037656ad22120bc431932b398433cf0c34]
    Former-commit-id: 2043a8ad0016ebbb892eb503190d66c8a43b10a3

---
+++ rpmbuild/SOURCES/eFa-4.0.0/eFaInit/src/AppBundle/Form/InterfaceEditTaskType.php
@@ -0,0 +1,36 @@
+<?php
+// src/AppBundle/Form/InterfaceEditTaskType.php
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\eFaInitTask;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class InterfaceEditTaskType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('Interface', ChoiceType::class, array(
+                'choices'  => $options['varChoices'],
+                'expanded' => false,
+                'multiple' => false,
+            ))
+            ->add('Save', SubmitType::class)
+        ;
+    }
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => eFaInitTask::class,
+            'csrf_token_id' => 'interface_task'
+        ));
+        $resolver->setRequired('varProperty');
+        $resolver->setRequired('varChoices');
+    }
+}
+?>
