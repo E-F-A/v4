0e0d22eae9b90228496aab2ab3cebaf0e6a7b5b2
Shawn Iverson <shawniverson@efa-project.org>
2018-07-08 07:51:17 -0400

    fix case on MailWatch package
    Former-commit-id: 2d260ec93df275a3c4966975563dfc5220e2d032 [formerly 3703ad42fc3c5494a85124090da5f8b5cec76079 [formerly ec745cd7f3e5836de63e8c49fbab3b4338f7c928]]
    Former-commit-id: 3703ad42fc3c5494a85124090da5f8b5cec76079
    Former-commit-id: f0533e860d027521205fdcdf504a94fdde5862b3

--- build/devbuild.sh
+++ build/devbuild.sh
@@ -114,9 +114,9 @@ rpmbuild -ba perl-Encoding-FixLatin.spec
 [ $? != 0 ] && exit 1
 yum -y install $GITPATH/rpmbuild/RPMS/x86_64/perl-Encoding-FixLatin-1.04-1.eFa.el7.x86_64.rpm
 [ $? != 0 ] && exit 1
-rpmbuild -ba mailwatch-1.2.9.spec
+rpmbuild -ba MailWatch-1.2.9.spec
 [ $? != 0 ] && exit 1
-yum -y install $GITPATH/rpmbuild/RPMS/x86_64/mailwatch-1.2.9-1.eFa.el7.x86_64.rpm
+yum -y install $GITPATH/rpmbuild/RPMS/x86_64/MailWatch-1.2.9-1.eFa.el7.x86_64.rpm
 [ $? != 0 ] && exit 1
 cd $GITPATH/rpmbuild/SOURCES
 [ $? != 0 ] && exit 1
