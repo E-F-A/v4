eb8d25353ba57962b0998dac406bb2a080d89849
Shawn Iverson <shawniverson@efa-project.org>
2018-07-08 13:52:59 -0400

    More config updates
    Former-commit-id: b4566ac7eb38f9a4a68511d8418e745fa04867a8

--- rpmbuild/SOURCES/eFa-4.0.0/clamav-unofficial-sigs-config-4.0.0.sh
+++ rpmbuild/SOURCES/eFa-4.0.0/clamav-unofficial-sigs-config-4.0.0.sh
@@ -29,9 +29,9 @@ source /usr/src/eFa/eFa-settings.inc
 #-----------------------------------------------------------------------------#
 sed -i '/^#user_configuration_complete="yes"/s/^#//g' /etc/clamav-unofficial-sigs/user.conf
 sed -i '/^ExecStart=/ c\ExecStart=/usr/sbin/clamav-unofficial-sigs.sh' /usr/lib/systemd/system/clamav-unofficial-sigs.service
-sed -i '/^WantedBy=/ c\WantedBy=clam.scan.service'  /usr/lib/systemd/system/clamav-unofficial-sigs.timer
-sed -i '/^clamd_restart_opt=/ c\clamd_restart_opt="systemctl restart clam.scan"' /etc/clamav-unofficial-sigs/os.conf
-sed -i '/^clamd_reload_opt=/ c\clamd_reload_opt=="clamdscan --config-file=/etc/clamd.d/clamd.conf --reload"' /etc/clamav-unofficial-sigs/os.conf
+sed -i '/^WantedBy=/ c\WantedBy=clamd@scan.service'  /usr/lib/systemd/system/clamav-unofficial-sigs.timer
+sed -i '/^clamd_restart_opt=/ c\clamd_restart_opt="systemctl restart clamd@scan"' /etc/clamav-unofficial-sigs/os.conf
+sed -i '/^clamd_reload_opt=/ c\clamd_reload_opt=="clamdscan --config-file=/etc/clamd.d/scan.conf --reload"' /etc/clamav-unofficial-sigs/os.conf
 
 #-----------------------------------------------------------------------------#
 # Finalize the installation
