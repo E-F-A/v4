b54ed55826d93d02f48d5cc88eb670e757344529
Shawn Iverson <shawniverson@efa-project.org>
2018-09-02 11:59:07 -0400

    header_checks during init
    More specific to remove only local entry.
    
    
    Former-commit-id: 7b5d942356e548004369f99191e5d145b95f8718
    Former-commit-id: c32105a9f80040787e8fcbe0590be961343730bd
    Former-commit-id: 97b5c5f56326c1e7f83cc09b430a98dc6773c7b3

--- rpmbuild/SOURCES/eFa-4.0.0/postfix-config-4.0.0.sh
+++ rpmbuild/SOURCES/eFa-4.0.0/postfix-config-4.0.0.sh
@@ -87,9 +87,9 @@ postconf -e "smtpd_recipient_restrictions = reject_unauth_destination, reject_no
 postconf -e "masquerade_domains = \$mydomain"
 postconf -e "smtpd_milters = inet:127.0.0.1:33333"
 
-# Hide localhost
-echo '/^Received:.*\(localhost\ \[127.0.0.1/ IGNORE' >> /etc/postfix/header_checks
-echo '/^Received:.*\(localhost\ \[::1/ IGNORE' >> /etc/postfix/header_checks
+# Hide localhost (moved to init phase)
+#echo '/^Received:.*\(localhost\ \[127.0.0.1/ IGNORE' >> /etc/postfix/header_checks
+#echo '/^Received:.*\(localhost\ \[::1/ IGNORE' >> /etc/postfix/header_checks
 
 #other configuration files
 newaliases
