64554e279c45204582aa3a8c92b70b0b7d0beb1a
Shawn Iverson <shawniverson@efa-project.org>
2018-12-29 18:57:50 -0500

    10 Updates
    eFa-Backup cron jobs for v4
    Spawn mailwatch relay scripts
    Fix paths for eFa-SA-Update
    Add confined selinux type for /var/eFa
    eFa3 migration integration
    eFa4 restore
    Update validators translations
    Add custom username or email validation
    Use custom validation for web username field
    Update MailWatch
    
    
    Former-commit-id: fd6f5745d2dce896071dc329ef5847fbd1095d2e [formerly ba9d8039a72821dada0aaa03310b9808f7fae978]
    Former-commit-id: 884fabc73d36fd62e80a9cfd90728e68717c273a

---
+++ rpmbuild/SOURCES/eFa-4.0.0/eFaInit/src/AppBundle/Validator/Constraints/UsernameOrEmailValidator.php
@@ -0,0 +1,36 @@
+<?php
+
+// src/AppBundle/Validator/Constraints/UsernameOrEmailValidator.php
+namespace AppBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+
+class UsernameOrEmailValidator extends ConstraintValidator
+{
+    public function validate($value, Constraint $constraint)
+    {
+        if (!$constraint instanceof UsernameOrEmail) {
+            throw new UnexpectedTypeException($constraint, UsernameOrEmail::class);
+        }
+
+        // custom constraints should ignore null and empty values to allow
+        // other constraints (NotBlank, NotNull, etc.) take care of that
+        if (null === $value || '' === $value) {
+            return;
+        }
+
+        if (!is_string($value)) {
+            throw new UnexpectedTypeException($value, 'string');
+        }
+        
+        if (!preg_match('/^[a-z_][a-z0-9_-]{1,30}+$/', $value, $matches) && !filter_var($value, FILTER_VALIDATE_EMAIL)) {
+            $this->context->buildViolation($constraint->message)
+                ->setParameter('{{ string }}', $value)
+                ->addViolation();
+        }
+    }
+}
+
+?>
