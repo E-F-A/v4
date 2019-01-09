ed27790b4ef9995dbe50389d93446f0b40b67de1
Shawn Iverson <shawniverson@efa-project.org>
2019-01-09 17:53:28 -0500

    3 Updates
    Add missing parameters for txrep db access
    Fix syntax in func_systemrestore
    Remove awl_mysql.sql no longer used
    
    
    Former-commit-id: 7ad46e3de1b87d2759b2f9cdc6d7a29c6009026f

--- rpmbuild/SOURCES/eFa-4.0.0/mariadb/awl_mysql.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE awl (
-  username varchar(100) NOT NULL default '',
-  email varbinary(255) NOT NULL default '',
-  ip varchar(40) NOT NULL default '',
-  count int(11) NOT NULL default '0',
-  totscore float NOT NULL default '0',
-  ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (username,email,ip)
-) ENGINE=InnoDB;
