ff6a2c5923b8971fdde534460cc1d4fcbbc5681a
Shawn Iverson <shawniverson@efa-project.org>
2019-01-18 06:24:15 -0500

    Move freshclam to build phase

--- rpmbuild/SOURCES/eFa-4.0.0/clamav-config-4.0.0.sh
+++ rpmbuild/SOURCES/eFa-4.0.0/clamav-config-4.0.0.sh
@@ -44,5 +44,7 @@ semanage fcontext -a -t antivirus_log_t /var/log/clamd.scan
 chown -R clamscan:mtagroup /var/run/clamd.scan
 echo "d /var/run/clamd.scan 0750 clamscan mtagroup -" > /etc/tmpfiles.d/clamd.scan.conf
 
+freshclam
+
 echo "Configuring clamav...done"
 
