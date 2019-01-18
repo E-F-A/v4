a8fa925ee688d58a0933a6c481a88f4b5810c495
Shawn Iverson <shawniverson@efa-project.org>
2019-01-17 19:09:01 -0500

    eFaInit GUI CLI username validation
    Former-commit-id: 88d771e9a06cdb195fe51b93153d7d488ddf12cc [formerly a55e8188d6490b6754489d37ad99e13a32bf98fc]
    Former-commit-id: f85e00d1f9fdbea0f9602faed2ac7bf3d7b9a507

---
+++ rpmbuild/SOURCES/eFa-4.0.0/eFaInit/src/AppBundle/Validator/Constraints/CLIUsername.php
@@ -0,0 +1,16 @@
+<?php 
+
+// src/AppBundle/Validator/Constraints/CLIUsername.php
+namespace AppBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @Annotation
+ */
+class CLIUsername extends Constraint
+{
+    public $message = 'CLI Username already exists or is not alphanumeric (up to 30 characters starting with a letter or _), please choose a different name.';
+}
+
+?>
---
+++ rpmbuild/SOURCES/eFa-4.0.0/eFaInit/src/AppBundle/Validator/Constraints/CLIUsernameValidator.php
@@ -0,0 +1,41 @@
+<?php
+
+// src/AppBundle/Validator/Constraints/CLIUsernameValidator.php
+namespace AppBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+
+class CLIUsernameValidator extends ConstraintValidator
+{
+    public function validate($value, Constraint $constraint)
+    {
+        if (!$constraint instanceof CLIUsername) {
+            throw new UnexpectedTypeException($constraint, CLIUsername::class);
+        }
+
+        // custom constraints should ignore null and empty values to allow
+        // other constraints (NotBlank, NotNull, etc.) take care of that
+        if (null === $value || '' === $value) {
+            return;
+        }
+
+        if (!is_string($value)) {
+            throw new UnexpectedTypeException($value, 'string');
+        }
+        
+        // Test for existing user account
+        $process = new Process("sudo cat /etc/passwd | awk -F\":\" '{print $1}' | grep -e \"^" . $value ."\"");
+        $process->mustRun();
+        $output = $process->getOutput();
+
+        if (!preg_match('/^[a-z_][a-z0-9_-]{1,30}+$/', $value, $matches) || !empty($output)) {
+            $this->context->buildViolation($constraint->message)
+                ->setParameter('{{ string }}', $value)
+                ->addViolation();
+        }
+    }
+}
+
+?>
