880cba6572609b8231473fcd8dbc515c4d24b4aa
Rob Asher <deepwoodscalls@gmail.com>
2019-03-14 10:58:37 -0500

    Modified build script to use curl instead of wget

--- build/build.bash
+++ build/build.bash
@@ -1,7 +1,6 @@
 #!/bin/bash
-action=$1
 #-----------------------------------------------------------------------------#
-# eFa 4.0.0 build script version 20170122
+# eFa 4.0.0 build script version 20190312
 #-----------------------------------------------------------------------------#
 # Copyright (C) 2013~2017 https://efa-project.org
 #
@@ -18,60 +17,117 @@ action=$1
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #-----------------------------------------------------------------------------#
+action=$1
+action="${action:=testing}" # default to testing if no arg supplied
 
 #-----------------------------------------------------------------------------#
 # Install eFa
 #-----------------------------------------------------------------------------#
 mirror="https://mirrors.efa-project.org"
 
+LOGFILE="/var/log/eFa/build.log"
+
+
+#==============================================================================================
+#
+# Set up logging
+#
+
+LOGGER='/usr/bin/logger'
+HEADER='=============  EFA4 BUILD SCRIPT STARTING  ============'
+
+# CREATE LOG FOLDER IF NOT EXISTS
+mkdir -p $(dirname "${LOGFILE}")
+
+# TRY TO CREATE LOG FILE IF NOT EXISTS
+( [ -e "$LOGFILE" ] || touch "$LOGFILE" ) && [ ! -w "$LOGFILE" ] && echo "Unable to create or write to $LOGFILE"
+
+function logthis() {
+    TAG='EFA4'
+    MSG="$1"
+    $LOGGER -t "$TAG" "$MSG"
+    echo "`date +%Y.%m.%d-%H:%M:%S` - $MSG"
+    echo "`date +%Y.%m.%d-%H:%M:%S` - $MSG" >> $LOGFILE
+}
+
+logthis "$HEADER"
+
 # check if user is root
 if [ `whoami` == root ]; then
-  echo "Good you are root."
+  logthis "Good you are root."
 else
-  echo "ERROR: Please become root first."
+  logthis "ERROR: Please become root first."
+  logthis "^^^^^^^^^^ SCRIPT ABORTED ^^^^^^^^^^"
   exit 1
 fi
 
 # check if we run CentOS 7
 OSVERSION=`cat /etc/centos-release`
 if [[ $OSVERSION =~ .*'release 7.'.* ]]; then
-  echo "- Good you are running CentOS 7"
+  logthis "Good you are running CentOS 7"
 else
-  echo "- ERROR: You are not running CentOS 7"
-  echo "- ERROR: Unsupported system, stopping now"
+  logthis "ERROR: You are not running CentOS 7"
+  logthis "ERROR: Unsupported system, stopping now"
+  logthis "^^^^^^^^^^ SCRIPT ABORTED ^^^^^^^^^^"
   exit 1
 fi
 
 # Check that SELinux is not disabled (unless it is lxc)
-if [[ -f /etc/selinux/config && -n $(grep -i ^SELINUX=disabled$ /etc/selinux/config)  ]]; then
-  echo "- ERROR: SELinux is disabled and this is not an lxc container"
-  echo "- ERROR: Please enable SELinux and try again."
-  exit 1
+if [[ -z $(grep -i 'lxc\|docker' /proc/1/cgroup) ]]; then
+    if [[ -f /etc/selinux/config && -n $(grep -i ^SELINUX=disabled$ /etc/selinux/config)  ]]; then
+        logthis "ERROR: SELinux is disabled and this is not an lxc container"
+        logthis "ERROR: Please enable SELinux and try again."
+        logthis "^^^^^^^^^^ SCRIPT ABORTED ^^^^^^^^^^"
+        exit 1
+    fi
 fi
 
 # Check network connectivity
-echo "- Checking network connectivity"
-wget -q --tries=3 --timeout=20 --spider $mirror
+logthis "Checking network connectivity"
+# use curl to test in case wget not installed yet.
+curl -s --connect-timeout 3 --max-time 5 --retry 3 --retry-delay 0 --retry-max-time 30 "${mirror}" > /dev/null
 if [[ $? -eq 0 ]]; then
-  echo "-- OK $mirror is reachable"
+  logthis "OK - $mirror is reachable"
 else
-  echo "ERROR: No network connectivity"
-  echo "ERROR: unable to reach $mirror"
-  echo "ERROR: Aborting script"
+  logthis "ERROR: No network connectivity"
+  logthis "ERROR: unable to reach $mirror"
+  logthis "^^^^^^^^^^ SCRIPT ABORTED ^^^^^^^^^^"
   exit 1
 fi
 
+# have network, install wget if missing
+rpm -q wget >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    logthis "Installing wget"
+    yum -y install wget
+    if [ $? -eq 0 ]; then
+        logthis "wget installed"
+    else
+        logthis "ERROR: wget installation failed"
+        # non-fatal for this script but will cause issues after system configuration
+    fi
+fi
+
 # Add eFa Repo
-if [[ "$action" == "testing" || "$action" == "kstesting" || "$action"="testingnoefa" ]]; then
-  echo "- Adding eFa Testing Repo"
-  rpm --import $mirror/rpm/eFa4/RPM-GPG-KEY-eFa-Project
-  /usr/bin/wget -O /etc/yum.repos.d/eFa4-testing.repo $mirror/rpm/eFa4/eFa4-testing.repo
-else
-  echo "- Adding eFa Repo"
-  rpm --import $mirror/rpm/eFa4/RPM-GPG-KEY-eFa-Project
-  cd /etc/yum.repos.d/
-  /usr/bin/wget $mirror/rpm/eFa4/eFa4.repo
-fi  
+aCTN=(testing kstesting testingnoefa)
+
+case "${aCTN[@]}" in
+    ("$action "*|*" $action "*|*" $action")
+       if [ ! -f /etc/yum.repos.d/eFa4-testing.repo ]; then
+           logthis "Adding eFa Testing Repo"
+           rpm --import $mirror/rpm/eFa4/RPM-GPG-KEY-eFa-Project
+           curl -L $mirror/rpm/eFa4/eFa4-testing.repo -o /etc/yum.repos.d/eFa4-testing.repo
+       fi
+       ;;
+
+    *) if [ ! -f /etc/yum.repos.d/eFa4.repo ]; then
+           logthis "Adding eFa Repo"
+           rpm --import $mirror/rpm/eFa4/RPM-GPG-KEY-eFa-Project
+           curl -L $mirror/rpm/eFa4/eFa4.repo -o /etc/yum.repos.d/eFa4.repo
+       fi
+       ;;
+esac
+
 
 # echo "- Adding mariadb Repo"
 # cat > /etc/yum.repos.d/mariadb.repo << 'EOF'
@@ -85,28 +141,58 @@ fi
 # enabled=1
 # EOF
 
-echo "- Adding epel Repo"
-yum -y install epel-release
-[ $? != 0 ] && exit 1
+rpm -q epel-release >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    logthis "Installing EPEL Repo"
+    yum -y install epel-release
+    if [ $? -eq 0 ]; then
+        logthis "EPEL repo installed"
+    else
+        logthis "ERROR: EPEL installation failed"
+        logthis "^^^^^^^^^^ SCRIPT ABORTED ^^^^^^^^^^"
+        exit 1
+    fi
+fi
 
-echo "- Adding IUS Repo"
-yum -y install https://centos7.iuscommunity.org/ius-release.rpm
-[ $? != 0 ] && exit 1
-rpm --import /etc/pki/rpm-gpg/IUS-COMMUNITY-GPG-KEY
-[ $? != 0 ] && exit 1
+rpm -q ius-release >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    logthis "Installing IUS Repo"
+    yum -y install https://centos7.iuscommunity.org/ius-release.rpm
+    if [ $? -eq 0 ]; then
+        logthis "IUS repo installed"
+        rpm --import /etc/pki/rpm-gpg/IUS-COMMUNITY-GPG-KEY
+    else
+        logthis "ERROR: IUS installation failed"
+        logthis "^^^^^^^^^^ SCRIPT ABORTED ^^^^^^^^^^"
+        exit 1
+    fi
+fi
 
-echo "- Updating the OS"
+logthis "Updating the OS"
 yum -y update
-[ $? != 0 ] && exit 1
+if [ $? -eq 0 ]; then
+    logthis "System Updated"
+else
+    logthis "+ $?"
+fi
 
-echo "- Removing conflicting packages"
-yum -y remove postfix mariadb-libs
-# Ignore return here 
+logthis "Removing conflicting packages"
+yum -y remove postfix mariadb-libs >/dev/null 2>&1
+# Ignore return here
 
 # install eFa
-if [[ "$action" != "testingnoefa" ]]; then
-  yum -y install eFa
-  [ $? != 0 ] && exit 1
+rpm -q eFa >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    if [[ "$action" != "testingnoefa" ]]; then
+        yum -y install eFa
+        if [ $? -eq 0 ]; then
+            logthis "eFa4 Installed"
+        else
+            logthis "ERROR: eFa4 failed to install"
+            logthis "^^^^^^^^^^ SCRIPT ABORTED ^^^^^^^^^^"
+            exit 1
+        fi
+    fi
 fi
 
 if [[ "$action" == "kstesting" || "$action" == "ksproduction" ]]; then
@@ -117,3 +203,15 @@ if [[ "$action" == "kstesting" || "$action" == "ksproduction" ]]; then
   # Disable ssh for kickstart builds
   systemctl disable sshd
 fi
+
+logthis "============  EFA4 BUILD SCRIPT FINISHED  ============"
+
+while true; do
+    read -p "Do you wish to reboot the system now?" yn
+    case $yn in
+        [Yy]* ) shutdown -r +1 "Installation requires reboot. Restarting in 1 minute"; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+exit 0
