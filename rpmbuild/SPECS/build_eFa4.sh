2552e39edb0f4eb92924ccaa17bfa2633bc9317f
Shawn Iverson <shawniverson@efa-project.org>
2019-12-27 09:27:49 -0500

    Update eFa build script
    Former-commit-id: 017ed95eda0db3d8bd4674d18bc6113a51855cc2
    Former-commit-id: 6a46e7c68ba73ea959930e0ab9782be83a865d2c

--- rpmbuild/SPECS/build_eFa4.sh
+++ rpmbuild/SPECS/build_eFa4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ../SOURCES/
-rm -f eFa-4.0.0.tar.gz
-tar czvf eFa-4.0.0.tar.gz eFa-4.0.0
+rm -f eFa-4.0.1.tar.gz
+tar czvf eFa-4.0.1.tar.gz eFa-4.0.1
 cd ../SPECS
 rpmbuild -ba eFa4.spec
