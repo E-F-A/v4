ecf39a31f5884394b8e642ea2e71cab5576d4cff
Shawn Iverson <shawniverson@efa-project.org>
2020-01-12 07:39:03 -0500

    Cleanup and copyright update

--- build/ansible/devbuild.sh
+++ build/ansible/devbuild.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #-----------------------------------------------------------------------------#
-# eFa 4.0.0 testbuild.sh version 20180204
+# eFa 4.0.1 testbuild.sh version 20180204
 #-----------------------------------------------------------------------------#
-# Copyright (C) 2013~2018 https://efa-project.org
+# Copyright (C) 2013~2020 https://efa-project.org
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
--- build/devbuild.sh
+++ build/devbuild.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ######################################################################
-# eFa 4.0.0 Build script for local development builds
+# eFa 4.0.1 Build script for local development builds
 ######################################################################
-# Copyright (C) 2018  https://efa-project.org
+# Copyright (C) 2020  https://efa-project.org
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
