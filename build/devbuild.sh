793d80562e90592d8ff50cbdff5c1ca346c2c397
Shawn Iverson <shawniverson@efa-project.org>
2020-01-25 12:00:15 -0500

    Generalize devbuild.sh

--- build/devbuild.sh
+++ build/devbuild.sh
@@ -18,6 +18,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #######################################################################
 
+VERSION="4.0.1"
+
 # Git path
 GITPATH="/root/v4"
 
@@ -87,35 +89,35 @@ yum -y install $GITPATH/rpmbuild/RPMS/x86_64/clamav-unofficial-sigs-*.rpm
 [ $? -ne 0 ] && exit 1
 echo "n" | rpmbuild -ba perl-libnet.spec
 [ $? -ne 0 ] && exit 1
-yum -y install $GITPATH/rpmbuild/RPMS/x86_64/perl-libnet-3.11-1.eFa.el7.x86_64.rpm
+yum -y install $GITPATH/rpmbuild/RPMS/x86_64/perl-libnet-*.rpm
 [ $? -ne 0 ] && exit 1
 rpmbuild -ba perl-IP-Country.spec
 [ $? -ne 0 ] && exit 1
-yum -y install $GITPATH/rpmbuild/RPMS/x86_64/perl-IP-Country-2.28-1.eFa.el7.x86_64.rpm
+yum -y install $GITPATH/rpmbuild/RPMS/x86_64/perl-IP-Country-*.rpm
 [ $? -ne 0 ] && exit 1
 rpmbuild -ba perl-Text-Balanced.spec
 [ $? -ne 0 ] && exit 1
-yum -y install $GITPATH/rpmbuild/RPMS/x86_64/perl-Text-Balanced-2.03-1.eFa.el7.x86_64.rpm
+yum -y install $GITPATH/rpmbuild/RPMS/x86_64/perl-Text-Balanced-*.rpm
 [ $? -ne 0 ] && exit 1
 rpmbuild -ba perl-Mail-SPF-Query.spec
 [ $? -ne 0 ] && exit 1
-yum -y install $GITPATH/rpmbuild/RPMS/x86_64/perl-Mail-SPF-Query-1.999.1-1.eFa.el7.x86_64.rpm
+yum -y install $GITPATH/rpmbuild/RPMS/x86_64/perl-Mail-SPF-Query-*.rpm
 [ $? -ne 0 ] && exit 1
 rpmbuild -ba unrar-5.8.3.spec
 [ $? -ne 0 ] && exit 1
-yum -y install $GITPATH/rpmbuild/RPMS/x86_64/unrar-5.8.3-1.eFa.el7.x86_64.rpm
+yum -y install $GITPATH/rpmbuild/RPMS/x86_64/unrar-*.rpm
 [ $? -ne 0 ] && exit 1
 rpmbuild -ba Spamassassin.spec
 [ $? -ne 0 ] && exit 1
-yum -y install $GITPATH/rpmbuild/RPMS/x86_64/spamassassin-3.4.4.rc1-1.eFa.el7.x86_64.rpm
+yum -y install $GITPATH/rpmbuild/RPMS/x86_64/spamassassin-*.rpm
 [ $? -ne 0 ] && exit 1
 rpmbuild -ba perl-Encoding-FixLatin.spec
 [ $? -ne 0 ] && exit 1
-yum -y install $GITPATH/rpmbuild/RPMS/x86_64/perl-Encoding-FixLatin-1.04-1.eFa.el7.x86_64.rpm
+yum -y install $GITPATH/rpmbuild/RPMS/x86_64/perl-Encoding-*.rpm
 [ $? -ne 0 ] && exit 1
 rpmbuild -ba perl-Sendmail-PMilter.spec
 [ $? -ne 0 ] && exit 1
-yum -y install $GITPATH/rpmbuild/RPMS/x86_64/perl-Sendmail-PMilter-1.00-1.eFa.el7.x86_64.rpm
+yum -y install $GITPATH/rpmbuild/RPMS/x86_64/perl-Sendmail-PMilter-*.rpm
 [ $? -ne 0 ] && exit 1
 rpmbuild -ba MailWatch.spec
 [ $? -ne 0 ] && exit 1
@@ -123,25 +125,25 @@ yum -y install $GITPATH/rpmbuild/RPMS/x86_64/MailWatch-*.rpm
 [ $? -ne 0 ] && exit 1
 rpmbuild -ba sqlgreywebinterface.spec
 [ $? -ne 0 ] && exit 1
-yum -y install $GITPATH/rpmbuild/RPMS/x86_64/sqlgreywebinterface-1.1.9-2.eFa.el7.x86_64.rpm
+yum -y install $GITPATH/rpmbuild/RPMS/x86_64/sqlgreywebinterface-*.rpm
 [ $? -ne 0 ] && exit 1
 cd $GITPATH/rpmbuild/SOURCES
 [ $? -ne 0 ] && exit 1
-rm -f eFa-4.0.1.tar.gz
+rm -f eFa-$VERSION.tar.gz
 [ $? -ne 0 ] && exit 1
-tar czvf eFa-4.0.1.tar.gz eFa-4.0.1/
+tar czvf eFa-$VERSION.tar.gz eFa-$VERSION/
 [ $? -ne 0 ] && exit 1
 rm -f eFa-base-4.0.0.tar.gz
 [ $? -ne 0 ] && exit 1
 tar czvf eFa-base-4.0.0.tar.gz eFa-base-4.0.0/
 [ $? -ne 0 ] && exit 1
-yum -y install MailScanner-5.2.2-1.efa.1.rhel.noarch.rpm
+yum -y install MailScanner-*.rhel.noarch.rpm
 [ $? -ne 0 ] && exit 1
 cd $GITPATH/rpmbuild/SPECS
 [ $? -ne 0 ] && exit 1
 rpmbuild -ba dcc.spec
 [ $? -ne 0 ] && exit 1
-yum -y install $GITPATH/rpmbuild/RPMS/x86_64/dcc-2.3.167-1.eFa.el7.x86_64.rpm
+yum -y install $GITPATH/rpmbuild/RPMS/x86_64/dcc-*.rpm
 [ $? -ne 0 ] && exit 1
 rpmbuild -ba eFa4-base.spec
 [ $? -ne 0 ] && exit 1
@@ -149,5 +151,5 @@ yum -y install $GITPATH/rpmbuild/RPMS/x86_64/eFa-base-*.rpm
 [ $? -ne 0 ] && exit 1
 rpmbuild -ba eFa4.spec
 [ $? -ne 0 ] && exit 1
-yum -y install $GITPATH/rpmbuild/RPMS/x86_64/eFa-4*rpm
+yum -y install $GITPATH/rpmbuild/RPMS/x86_64/eFa-4*.rpm
 [ $? -ne 0 ] && exit 1
