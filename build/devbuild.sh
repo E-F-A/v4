ae8d811782706816e37974b5321f4e362a2e54ac
Shawn Iverson <shawniverson@efa-project.org>
2020-02-02 10:25:42 -0500

    Add perl-GeoIP2-Country-Reader to devbuild
    Former-commit-id: 67d3dd53b92715114480a4db068114ac4f074505 [formerly 68ceb0b655c3f2890313503bfda0d5cb556d7198]
    Former-commit-id: efdc7e1c980daab6aaebb1fb0b470760df11d660

--- build/devbuild.sh
+++ build/devbuild.sh
@@ -192,6 +192,10 @@ rpmbuild -ba perl-Maxmind-DB-Reader.spec
 [ $? -ne 0 ] && exit 1
 yum -y install $GITPATH/rpmbuild/RPMS/x86_64/perl-Maxmind-DB-Reader-*.rpm
 [ $? -ne 0 ] && exit 1
+rpmbuild -ba perl-GeoIP2-Country-Reader.spec
+[ $? -ne 0 ] && exit 1
+yum -y install $GITPATH/rpmbuild/RPMS/x86_64/perl-GeoIP2-Country-Reader-*.rpm
+[ $? -ne 0 ] && exit 1
 # END: New modules for spamassassin 3.4.4 development builds
 rpmbuild -ba Spamassassin.spec
 [ $? -ne 0 ] && exit 1
