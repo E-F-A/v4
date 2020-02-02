39e837c27ea3f697a95e2c76df39d10d4c73eb3e
Shawn Iverson <shawniverson@efa-project.org>
2020-02-02 18:32:20 -0500

    Use ifplugin in spamassassin.conf Reapply php-fpm group during base install Enable RelayCountry plugin
    Former-commit-id: 7b476280841dcb1412c9326d2077eb319be3697d [formerly 1dedf4c3f809c4491cb8dfb5243c231a2562eff5]
    Former-commit-id: 0754299260e6f574cad7ac4452a3f44a71d72e20

--- rpmbuild/SOURCES/eFa-base-4.0.0/spamassassin-config-4.0.0.sh
+++ rpmbuild/SOURCES/eFa-base-4.0.0/spamassassin-config-4.0.0.sh
@@ -91,11 +91,19 @@ echo 'bayes_auto_learn_threshold_nonspam 0.1' >> /etc/MailScanner/spamassassin.c
 echo 'bayes_auto_learn_threshold_spam    6' >> /etc/MailScanner/spamassassin.conf
 
 # GeoIP2 County path
-echo 'uri_country_db_path /usr/share/GeoIP/GeoLite2-Country.mmdb' >> /etc/MailScanner/spamassassin.conf
-echo 'geoip2_default_db_path /usr/share/GeoIP/GeoLite2-Country.mmdb' >> /etc/MailScanner/spamassassin.conf
+echo '' >> etc/MailScanner/spamassassin.conf
+echo 'ifplugin Mail::SpamAssassin::Plugin::URILocalBL' >> /etc/MailScanner/spamassassin.conf
+echo '    uri_country_db_path /usr/share/GeoIP/GeoLite2-Country.mmdb' >> /etc/MailScanner/spamassassin.conf
+echo 'endif' >> /etc/MailScanner/spamassassin.conf
+cho 'ifplugin Mail::SpamAssassin::Plugin::RelayCountry' >> /etc/MailScanner/spamassassin.conf
+echo '    geoip2_default_db_path /usr/share/GeoIP/GeoLite2-Country.mmdb' >> /etc/MailScanner/spamassassin.conf
+echo 'endif' >> /etc/MailScanner/spamassassin.conf
 
 # Enable Auto White Listing
 #sed -i '/^#loadplugin Mail::SpamAssassin::Plugin::AWL/ c\loadplugin Mail::SpamAssassin::Plugin::AWL' /etc/mail/spamassassin/v310.pre
+
+# Enable RelayCountries Plugin
+sed -i "/^# loadplugin Mail::SpamAssassin::Plugin::RelayCountry/ c\loadplugin Mail::SpamAssassin::Plugin::RelayCountry" /etc/mail/spamassassin/init.pre
 
 # Enable TxRep Plugin
 sed -i "/^# loadplugin Mail::SpamAssassin::Plugin::TxRep/ c\loadplugin Mail::SpamAssassin::Plugin::TxRep" /etc/mail/spamassassin/v341.pre
