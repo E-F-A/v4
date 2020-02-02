150f4c982295d75a8b10a64d6b0238bb44d27da5
Shawn Iverson <shawniverson@efa-project.org>
2020-02-02 08:03:28 -0500

    Add perl-Maxmind-DB-Metadata to devbuild

--- build/devbuild.sh
+++ build/devbuild.sh
@@ -166,6 +166,10 @@ rpmbuild -ba perl-MooX-StrictConstructor.spec
 [ $? -ne 0 ] && exit 1
 yum -y install $GITPATH/rpmbuild/RPMS/x86_64/perl-MooX-StrictConstructor-*.rpm
 [ $? -ne 0 ] && exit 1
+rpmbuild -ba perl-Maxmind-DB-Metadata.spec
+[ $? -ne 0 ] && exit 1
+yum -y install $GITPATH/rpmbuild/RPMS/x86_64/perl-Maxmind-DB-Metadata-*.rpm
+[ $? -ne 0 ] && exit 1
 rpmbuild -ba Spamassassin.spec
 [ $? -ne 0 ] && exit 1
 yum -y install $GITPATH/rpmbuild/RPMS/x86_64/spamassassin-*.rpm
