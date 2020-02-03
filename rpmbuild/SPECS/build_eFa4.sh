b23457d50a3ae27254a5938eeb0a04f866cf13c9
Shawn Iverson <shawniverson@efa-project.org>
2020-02-02 20:42:43 -0500

    Repackage
    Former-commit-id: a746802383b679ff777bf50741dfc4d97faab3d0 [formerly fa46bbeae09a99432062fee2978d43512e0fe2a1]
    Former-commit-id: 36fe0babce8dceeb187250c4edc59bc5f40717b6

--- rpmbuild/SPECS/build_eFa4.sh
+++ rpmbuild/SPECS/build_eFa4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ../SOURCES/
-rm -f eFa-4.0.1.tar.gz
-tar czvf eFa-4.0.1.tar.gz eFa-4.0.1
+rm -f eFa-4.0.2.tar.gz
+tar czvf eFa-4.0.2.tar.gz eFa-4.0.2
 cd ../SPECS
 rpmbuild -ba eFa4.spec
