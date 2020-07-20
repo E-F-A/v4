cf97092218bfb66fedc6a01b4207153ee96862a2
Shawn Iverson <shawniverson@efa-project.org>
2020-07-19 21:40:20 -0400

    Suppress symlinking output
    Former-commit-id: 94fe54c24872013748e12558df4d1d3683fc2e81 [formerly bb433bdb9111d75858a23da431e57f1970ca7353]
    Former-commit-id: 07130cd35c8cc66ac7ce34c185390b1892c8ab2a

--- rpmbuild/SOURCES/eFa-4.0.2/updates/update-4.0.2.sh
+++ rpmbuild/SOURCES/eFa-4.0.2/updates/update-4.0.2.sh
@@ -152,7 +152,7 @@ if [[ -n $(grep "nameserver=127.0.0.1" /etc/resolv.conf) ]]; then
 fi
 
 # Ensure symlink is present
-ln -s /etc/MailScanner/spamassassin.conf /etc/mail/spamassassin/mailscanner.cf
+ln -s /etc/MailScanner/spamassassin.conf /etc/mail/spamassassin/mailscanner.cf  >/dev/null 2>&1
 
 # Fix 1x1 spacer
 sed -i "/^Web Bug Replacement = https:\/\/s3.amazonaws.com\/msv5\/images\/spacer.gif/ c\Web Bug Replacement = http://dl.efa-project.org/static/1x1spacer.gif" /etc/MailScanner/MailScanner.conf
