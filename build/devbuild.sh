6f6b1a775e3258e41adeaec5df313c4b935910f8
Shawn Iverson <shawniverson@efa-project.org>
2020-07-26 17:23:44 -0400

    noarch for perl-Data-Printer

--- build/devbuild.sh
+++ build/devbuild.sh
@@ -226,7 +226,7 @@ yum -y install $GITPATH/rpmbuild/RPMS/noarch/perl-Data-IEEE754-*.rpm
 [ $? -ne 0 ] && exit 1
 rpmbuild -ba perl-Data-Printer.spec
 [ $? -ne 0 ] && exit 1
-yum -y install $GITPATH/rpmbuild/RPMS/x86_64/perl-Data-Printer-*.rpm
+yum -y install $GITPATH/rpmbuild/RPMS/noarch/perl-Data-Printer-*.rpm
 [ $? -ne 0 ] && exit 1
 rpmbuild -ba perl-Data-Validate-IP.spec
 [ $? -ne 0 ] && exit 1
