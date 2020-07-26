133a5b2249cfb54c670621c8ceb43c2f05f42ea2
Shawn Iverson <shawniverson@efa-project.org>
2020-07-26 18:04:25 -0400

    update build script Add BuildRequires for perl-Inline Update BuildRequires for Spamassassin
    Former-commit-id: 309ac5c18f4ef58120ebf860f8ad831bfc9163ba [formerly 96930218abebf626edca729bd3e69b58651573bb]
    Former-commit-id: 97f80a730dc5d5d1124986a481d40a54de78585b

--- build/devbuild.sh
+++ build/devbuild.sh
@@ -105,7 +105,7 @@ else
     perl-Test-Fatal perl-Test-Number-Delta perl-namespace-autoclean perl-Role-Tiny perl-DateTime perl-Test-Warnings \
     perl-autodie perl-Test-Requires perl-Clone-PP perl-File-HomeDir perl-Sort-Naturally perl-JSON-MaybeXS \
     perl-LWP-Protocol-https perl-Test-LeakTrace perl-Throwable libmaxminddb-devel libdb-devel pcre-devel make \ 
-    libnsl2-devel perl-Test perl-Params-Validate perl
+    libnsl2-devel perl-Test perl-Params-Validate perl perl-Test-Warn
     [ $? -ne 0 ] && exit 1
 fi
 
@@ -255,11 +255,13 @@ yum -y install $GITPATH/rpmbuild/RPMS/x86_64/perl-MaxMind-DB-Reader-XS-*.rpm
 [ $? -ne 0 ] && exit 1
 [ $RELEASE -eq 8 ] && rpmbuild -ba perl-Business-ISBN-Data.spec
 [ $? -ne 0 ] && exit 1
-[ $RELEASE -eq 8 ] && yum -y install $GITPATH/rpmbuild/RPMS/noarch/perl-Business-ISBN-Data-*.rpm
-[ $? -ne 0 ] && exit 1
 [ $RELEASE -eq 8 ] && rpmbuild -ba perl-Business-ISBN.spec
 [ $? -ne 0 ] && exit 1
 [ $RELEASE -eq 8 ] && yum -y install $GITPATH/rpmbuild/RPMS/noarch/perl-Business-ISBN-*.rpm
+[ $? -ne 0 ] && exit 1
+[ $RELEASE -eq 8 ] && rpmbuild -ba perl-Inline.spec
+[ $? -ne 0 ] && exit 1
+[ $RELEASE -eq 8 ] && yum -y install $GITPATH/rpmbuild/RPMS/noarch/perl-Inline-*.rpm
 [ $? -ne 0 ] && exit 1
 # END: New modules for spamassassin 3.4.4 development builds
 rpmbuild -ba Spamassassin.spec
