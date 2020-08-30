ff18ca59c9a53ca7751485583f86c0429c65a8d3
Shawn Iverson <shawniverson@efa-project.org>
2020-08-29 20:53:37 -0400

    Update 4.0.3-1
    Former-commit-id: 4922556eae128e2f0ec660a381a8739965930abf

--- rpmbuild/SPECS/build_eFa4.sh
+++ rpmbuild/SPECS/build_eFa4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ../SOURCES/
-rm -f eFa-4.0.2.tar.gz
-tar czvf eFa-4.0.2.tar.gz eFa-4.0.2
+rm -f eFa-4.0.3.tar.gz
+tar czvf eFa-4.0.3.tar.gz eFa-4.0.3
 cd ../SPECS
 rpmbuild -ba eFa4.spec
