ed2f6c4d6ad7ea7716d49ea1243fd01f157fea2d
Shawn Iverson <shawniverson@efa-project.org>
2024-04-06 08:15:51 -0400

    Update MailScanner and MailWatch, fix opendkim install

---
+++ rpmbuild/SOURCES/eFa-base-4.0.0/opendkim-config-4.0.0.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#-----------------------------------------------------------------------------#
+# eFa 5.0.0 initial mailwatch-configuration script
+#-----------------------------------------------------------------------------#
+# Copyright (C) 2024 https://efa-project.org
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#-----------------------------------------------------------------------------#
+
+#-----------------------------------------------------------------------------#
+# Source the settings file
+#-----------------------------------------------------------------------------#
+source /usr/src/eFa/eFa-settings.inc
+#-----------------------------------------------------------------------------#
+
+sed -i "/^Socket / c\Socket inet:8891@localhost" /etc/opendkim.conf
+
